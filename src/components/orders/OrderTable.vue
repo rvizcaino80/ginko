6366b36caa7d95fcdb6fe6a4313d8275d77b3bf0
Rogers Vizcaino <rvizcaino@gmail.com>
2026-05-23 17:11:00 -0500

    fix: encabezado Monto alineado a la derecha con :headerStyle

--- src/components/orders/OrderTable.vue
+++ src/components/orders/OrderTable.vue
@@ -33,10 +33,7 @@ function formatDate(iso: string): string {
     >
       <Column field="id" header="ID" sortable style="min-width: 8rem" />
       <Column field="proveedor" header="Proveedor" sortable style="min-width: 14rem" />
-      <Column field="monto" sortable style="min-width: 10rem">
-        <template #header>
-          <div class="text-right">Monto</div>
-        </template>
+      <Column field="monto" header="Monto" sortable :headerStyle="{ textAlign: 'right' }" style="min-width: 10rem">
         <template #body="{ data }">
           <div class="text-right font-mono">{{ formatCOP(data.monto) }}</div>
         </template>
