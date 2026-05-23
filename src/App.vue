4e93dbadc73d7a9683a9b5a494e0ec79f87ffdca
Rogers Vizcaino <rvizcaino@gmail.com>
2026-05-23 15:34:00 -0500

    feat: sidebar IA ocupa toda la altura de la ventana (fixed top-0 h-screen)

--- src/App.vue
+++ src/App.vue
@@ -20,13 +20,13 @@ useKeyboardShortcuts({
 <template>
   <div class="min-h-screen flex flex-col">
     <AppHeader />
-    <div class="flex flex-1">
+    <div class="flex flex-1 xl:mr-[400px]">
       <main class="flex-1 min-w-0 p-6 overflow-auto">
         <RouterView />
       </main>
-      <aside class="hidden xl:flex w-[400px] min-w-[400px] border-l border-gray-200 dark:border-gray-800">
-        <AiAssistant />
-      </aside>
     </div>
   </div>
+  <aside class="hidden xl:flex fixed top-0 right-0 h-screen w-[400px] border-l border-gray-200 dark:border-gray-800 bg-white dark:bg-gray-900 z-30">
+    <AiAssistant />
+  </aside>
 </template>
